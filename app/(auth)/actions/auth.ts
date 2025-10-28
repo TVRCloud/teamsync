85e868e3b592c673328fce4e830f4bf5f39be855
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-10-28 18:25:49 +0530

    Update auth.ts

--- app/(auth)/actions/auth.ts
+++ app/(auth)/actions/auth.ts
@@ -1,9 +1,37 @@
+"use server";
+
+import connectDB from "@/lib/mongodb";
+import users from "@/models/users";
+import { hashPassword, setSession } from "@/utils/auth";
+import { redirect } from "next/navigation";
+
 export async function registerAction(formData: FormData) {
   const name = formData.get("name") as string;
   const email = formData.get("email") as string;
   const password = formData.get("password") as string;
 
-  console.log(name, email, password);
+  if (!name || !email || !password) {
+    return { message: "Name, email, and password are required" };
+  }
+
+  await connectDB();
+  const existingUser = await users.findOne({ email });
+
+  if (existingUser) {
+    return { message: "User already exists" };
+  }
+
+  const hashedPassword = await hashPassword(password);
+  const user = await users.create({
+    name,
+    email,
+    password: hashedPassword,
+    role: "guest",
+  });
+
+  await setSession(user);
+
+  redirect("/dashboard");
 }
 
 export async function loginAction(formData: FormData) {
