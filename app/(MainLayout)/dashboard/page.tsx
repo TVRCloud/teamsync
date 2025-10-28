ecc42defe489c44ab66d1d7bbf6376eaf568494a
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-10-28 15:21:25 +0530

    Create page.tsx

---
+++ app/(MainLayout)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import DashboardMain from "@/components/dashboard/DashboardMain";
+import LayoutWrap from "@/components/layout-wrap";
+
+const Dashboard = () => {
+  return (
+    <LayoutWrap>
+      <DashboardMain />
+    </LayoutWrap>
+  );
+};
+
+export default Dashboard;
