8011d614f50e7f800c27907ee6e4408f53ed32ee
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-10-29 15:21:02 +0530

    Create page.tsx

---
+++ app/(MainLayout)/members/page.tsx
@@ -0,0 +1,12 @@
+import LayoutWrap from "@/components/layout-wrap";
+import MembersMain from "@/components/members/MembersMain";
+
+const Members = () => {
+  return (
+    <LayoutWrap>
+      <MembersMain />
+    </LayoutWrap>
+  );
+};
+
+export default Members;
