2f2d13ac72d7e507fd3c0a8ccc7ead0cc89d38bc
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-11-03 09:05:20 +0530

    Update route.ts

--- app/api/log/[id]/route.ts
+++ app/api/log/[id]/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import connectDB from "@/lib/mongodb";
 import ActivityLog from "@/models/log";
+import { authenticateUser } from "@/lib/authenticateUser";
 
 export async function GET(
   request: NextRequest,
