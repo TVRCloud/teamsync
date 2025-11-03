2dfcd9fe5d49ad85e42dd73186acca2d3d859bfd
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-11-03 13:03:38 +0530

    Create page.tsx

---
+++ app/(MainLayout)/projects/completed/page.tsx
@@ -0,0 +1,7 @@
+import LayoutWrap from "@/components/layout-wrap";
+
+const CompletedProjects = () => {
+  return <LayoutWrap>CompletedProjects</LayoutWrap>;
+};
+
+export default CompletedProjects;
