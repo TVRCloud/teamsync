46cd1ddd0037a58dc72422a1a0f1c511be44dbad
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-11-03 13:03:41 +0530

    Create page.tsx

---
+++ app/(MainLayout)/projects/page.tsx
@@ -0,0 +1,12 @@
+import LayoutWrap from "@/components/layout-wrap";
+import ProjectsMain from "@/components/projects/ProjectsMain";
+
+const Projects = () => {
+  return (
+    <LayoutWrap>
+      <ProjectsMain />
+    </LayoutWrap>
+  );
+};
+
+export default Projects;
