36737997b736fdcea6865054c663919a69b175b2
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-11-04 17:46:16 +0530

    Update ViewMembersMain.tsx

--- components/members/ViewMembersMain.tsx
+++ components/members/ViewMembersMain.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import { useViewUser } from "@/hooks/useUser";
 import { useParams } from "next/navigation";
 
 const ViewMembersMain = () => {
   const params = useParams<{ id: string }>();
+
+  const { data } = useViewUser(params.id);
 
   return <div>Member {params.id}</div>;
 };
