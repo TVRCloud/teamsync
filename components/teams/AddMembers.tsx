4ca54f853535cb77313b31fab689f173707275f5
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-11-04 17:46:22 +0530

    Create AddMembers.tsx

---
+++ components/teams/AddMembers.tsx
@@ -0,0 +1,53 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useState } from "react";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import MultiSelect from "../ui/multiselect";
+import { useInfiniteUsers } from "@/hooks/useUser";
+import { UseFormReturn } from "react-hook-form";
+import { TCreateTeamSchema } from "@/schemas/teams";
+
+interface Props {
+  form: UseFormReturn<TCreateTeamSchema>;
+}
+
+const AddMembers = ({ form }: Props) => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const { data, isLoading } = useInfiniteUsers(searchQuery);
+  const filteredUsers = data?.pages.flat() || [];
+  return (
+    <>
+      {/* Members */}
+      <FormField
+        control={form.control}
+        name="members"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Members</FormLabel>
+            <FormControl>
+              <MultiSelect
+                value={field.value as string[]}
+                onChange={field.onChange}
+                options={filteredUsers.map((user: any) => ({
+                  label: user.name,
+                  value: user._id,
+                }))}
+                isLoading={isLoading}
+                onSearchChange={setSearchQuery}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    </>
+  );
+};
+
+export default AddMembers;
