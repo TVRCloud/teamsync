29d7112e6399547b703b23c6df59fce122a66035
Amegh <80452444+amegh-ts@users.noreply.github.com>
2025-11-05 16:50:35 +0530

    Create page.tsx

---
+++ app/(MainLayout)/profile/page.tsx
@@ -0,0 +1,12 @@
+import LayoutWrap from "@/components/layout-wrap";
+import ProfileMain from "@/components/profile/ProfileMain";
+
+const Profile = () => {
+  return (
+    <LayoutWrap>
+      <ProfileMain />
+    </LayoutWrap>
+  );
+};
+
+export default Profile;
